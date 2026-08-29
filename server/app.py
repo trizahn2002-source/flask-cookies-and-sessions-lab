1ee468b7b41535e46717cefc1c47bd5bae323d64
Trizah NJeri <trizahn2002@gmail.com>
2026-08-29 13:51:40 +0300

    Implement session-based paywell feature

--- server/app.py
+++ server/app.py
@@ -27,8 +27,16 @@ def index_articles():
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-    pass
+    if 'page_views' not in session:
+        session['page_views'] = 0
 
+    session['page_views'] += 1
+
+    if session['page_views'] <= 3:
+        article = Article.query.filter_by(id=id).first()
+        return make_response(ArticleSchema().dump(article), 200)
+    else:
+        return {'message': 'Maximum pageview limit reached'}, 401
 
 if __name__ == '__main__':
     app.run(port=5555)
